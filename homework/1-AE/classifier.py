b171c3684d3ff6d2c8ffbc97214e74040b16cfea
MaximArtemev <maksim-makim@yandex.ru>
2021-02-10 15:20:44 +0300

    hw-1
    Deadline: 10 + 14 feb

---
+++ homework/1-AE/classifier.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class Classifier(nn.Module):
+    def __init__(self, n_classes=10):
+        super().__init__()
+        # YOUR CODE
+        
+    def forward(self, x):
+        # YOUR CODE
+        return x
+    
+    def get_activations(self, x):
+        return # YOUR CODE
